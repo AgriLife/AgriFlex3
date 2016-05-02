4d115525cee2fa0132b545cd50b247b5b9128e47
Zachary Watkins <watkinza@gmail.com>
2016-05-02 11:29:27 -0500

    Resolve JS error due to previous modifications

--- bower_components/foundation/js/foundation/foundation.topbar.js
+++ bower_components/foundation/js/foundation/foundation.topbar.js
@@ -255,7 +255,8 @@
       });
 
       S('body').off('.topbar').on('click.fndtn.topbar', function (e) {
-        var parent = S(e.target).closest('li').closest('li.hover');
+        var parent = S(e.target).closest('li').closest('li.hover'),
+            settings = topbar.data(self.attr_name(true) + '-init');
 
         if (parent.length > 0) {
           return;
