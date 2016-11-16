93a1a632063dbb793fb6d9658b126a2b6284c145
Zachary Watkins <watkinza@gmail.com>
2016-11-16 15:14:10 -0600

    Use default WordPress search in place of Google Custom Search

--- search.php
+++ search.php
@@ -6,62 +6,142 @@
  * @subpackage AgriFlex3
  */
 
-// Get the search ID. Set default if not specified.
+// Remove search result meta data
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
-remove_action( 'genesis_loop', 'genesis_do_loop' );
-add_action( 'genesis_loop', 'agriflex_do_search' );
-
-function agriflex_do_search() {
-
-	$search_id = get_field( 'google-search-id' );
-	if ( empty( $search_id ) ) {
-		$search_id = '000100048838736456753:esay0djx0qo';
-	}
-	ob_start();
-	?>
-	<div id='cse' style='width: 100%;'>Loading</div>
-	<script src='//www.google.com/jsapi' type='text/javascript'></script>
-	<script type='text/javascript'>
-		google.load('search', '1', {language: 'en', style: google.loader.themes.V2_DEFAULT});
-		google.setOnLoadCallback(function () {
-			var customSearchOptions = {};
-			var orderByOptions = {};
-			orderByOptions['keys'] = [
-				{label: 'Relevance', key: ''} ,
-				{label: 'Date', key: 'date'}
-			];
-			customSearchOptions['enableOrderBy'] = true;
-			customSearchOptions['orderByOptions'] = orderByOptions;
-			var customSearchControl = new google.search.CustomSearchControl('<?php echo $search_id; ?>', customSearchOptions);
-			customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-			var options = new google.search.DrawOptions();
-			options.enableSearchResultsOnly();
-			options.setAutoComplete(true);
-			customSearchControl.draw('cse', options);
-			function parseParamsFromUrl() {
-				var params = {};
-				var parts = window.location.search.substr(1).split('&');
-				for (var i = 0; i < parts.length; i++) {
-					var keyValuePair = parts[i].split('=');
-					var key = decodeURIComponent(keyValuePair[0]);
-					params[key] = keyValuePair[1] ?
-						decodeURIComponent(keyValuePair[1].replace(/\+/g, ' ')) :
-						keyValuePair[1];
-				}
-				return params;
-			}
-
-			var urlParams = parseParamsFromUrl();
-			var queryParamName = 's';
-			if (urlParams[queryParamName]) {
-				customSearchControl.execute(urlParams[queryParamName]);
-			}
-		}, true);
-	</script>
-	<?php
-	$output = ob_get_clean();
-	echo $output;
-
+// Restrict excerpt length
+add_filter( 'excerpt_length', 'sp_excerpt_length' );
+function sp_excerpt_length( $length ) {
+	return 50;
 }
+
+// Ensure search results use excerpts
+add_filter( 'genesis_pre_get_option_content_archive', 'show_excerpts' );
+function show_excerpts() {
+	return 'excerpts';
+}
+
+// Format search result information
+function filter_function_name( $excerpt ) {
+
+	$type = get_post_type();
+	if($type == 'people'){
+		// People post types don't have an excerpt, so it's built here.
+		// Based on single-people.php from AgriLife People plugin.
+		$parts = array();
+
+		$title = get_field('ag-people-title');
+		if($title)
+			$parts[] = 'Title: ' . $title;
+
+		$office = get_field('ag-people-office-location');
+		if($office)
+			$parts[] = 'Office: ' . $office;
+
+		$email = get_field('ag-people-email');
+		if($email)
+			$parts[] = 'Email: ' . $email;
+
+		$phone = get_field('ag-people-phone');
+		if($phone)
+			$parts[] = 'Phone: ' . $phone;
+
+		$resume = get_field('ag-people-resume');
+		if($resume)
+			$parts[] = 'Resume/CV: ' . $resume;
+
+		$website = get_field('ag-people-website');
+		if($website)
+			$parts[] = $website;
+
+		$undergrad_items = array();
+    if ( get_field( 'ag-people-undergrad' ) ) {
+        while ( has_sub_field( 'ag-people-undergrad' ) ) :
+            $undergrad_items[] = get_sub_field( 'ag-people-undergrad-degree' );
+        endwhile;
+    }
+    $undergrad = implode(',', $undergrad_items);
+		if(!empty($undergrad))
+			$parts[] = 'Undergraduate Education: ' . $undergrad;
+
+    $grad_items = array();
+    if ( get_field( 'ag-people-graduate' ) ) {
+        while ( has_sub_field( 'ag-people-graduate' ) ) :
+            $grad_items[] = get_sub_field( 'ag-people-graduate-degree' );
+        endwhile;
+    }
+    $grad = implode(',', $grad_items);
+		if(!empty($grad))
+			$parts[] = 'Graduate Education: ' . $grad;
+
+    $award_items = array();
+    if ( get_field( 'ag-people-awards' ) ) {
+        while ( has_sub_field( 'ag-people-awards' ) ) :
+            $award_items[] = get_sub_field( 'ag-people-award' );
+        endwhile;
+    }
+    $award = implode(', ', $award_items);
+		if(!empty($award))
+			$parts[] = 'Awards: ' . $award;
+
+    $taught_items = array();
+    if ( get_field( 'ag-people-courses' ) ) {
+        while ( has_sub_field( 'ag-people-courses' ) ) :
+            $taught_items[] = get_sub_field( 'ag-people-course' );
+        endwhile;
+    }
+    $taught = implode(', ', $taught_items);
+		if(!empty($taught))
+			$parts[] = 'Courses Taught: ' . $taught;
+
+    $content_items = array();
+    while ( has_sub_field( 'ag-people-content' ) ) :
+        $layout = get_row_layout();
+        switch( $layout ) {
+            case 'ag-people-content-header' :
+                $content_items[] = get_sub_field( 'header' );
+                break;
+            case 'ag-people-content-text' :
+                $content_items[] = get_sub_field( 'text' );
+                break;
+            default :
+            		break;
+        }
+    endwhile;
+    $content = implode(', ', $content_items);
+		if(!empty($content))
+			$parts[] = $content;
+
+		// Concatenate people data to string
+		$excerpt = implode( '; ', $parts );
+		// Remove HTML
+		$excerpt = wp_strip_all_tags( $excerpt );
+		// Replace special whitespace characters with normal spaces
+		$excerpt = preg_replace( '/[\s\t\r\R\v]+/', ' ', $excerpt );
+		$excerpt = preg_replace( '/[^\w\d,.;"\')]?\h\s/', ' ', $excerpt );
+		// Combine all people data for excerpt
+		$excerpt = preg_replace( '/ ;/', ';', $excerpt );
+
+	}
+
+	if(strpos($excerpt, 'class="read-more"') === false){
+		// Read more link not included with excerpt
+		$excerpt .= '<span class="read-more"><a href="' . get_permalink() . '">Read More →</a></span>';
+	}
+
+  return $excerpt;
+}
+add_filter( 'get_the_excerpt', 'filter_function_name' );
+
+// Add search results page title
+add_action( 'genesis_before_loop', function() {
+
+  $title = sprintf( '<div class="entry-header"><h1 class="entry-title">Search Results for: %s</h1></div>',
+    get_search_query()
+  );
+
+  echo $title;
+
+});
 
 genesis();
